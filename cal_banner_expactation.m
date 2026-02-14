2adfb7fbc6a5f60cd8e5d16948122d4d38dafeea
Runze Tian <167693983+t-runze@users.noreply.github.com>
2026-02-14 15:45:13 +0100

    update
    修正必中抽数非整数的bug
    加入cdf可视化

--- cal_banner_expactation.m
+++ cal_banner_expactation.m
@@ -1,21 +1,35 @@
 %% init%
 expactation = 0;
-banner.prob_basic = 0.02;
-banner.prob_StepWidth = 0.02;
-banner.prob_IncreaseNum = 50;
-banner.prob_must = banner.prob_IncreaseNum + (1-banner.prob_basic)/banner.prob_StepWidth;
-prob = zeros(banner.prob_must);
-
+banner.prob_basic = 0.006;
+banner.prob_StepWidth = 0.06;
+banner.prob_IncreaseNum = 73;
+banner.prob_must = ceil(banner.prob_IncreaseNum + (1-banner.prob_basic)/banner.prob_StepWidth);
+pdf = zeros(banner.prob_must);
+cdf = zeros(banner.prob_must);
 
 %% calculation
 for i = 1:banner.prob_must
-    prob(i) = cal_prob( i , banner.prob_basic , banner.prob_IncreaseNum , banner.prob_StepWidth);
-    expactation = expactation + i * prob(i);
+    pdf(i) = cal_prob( i , banner.prob_basic , banner.prob_IncreaseNum , banner.prob_StepWidth);
+    if i==1
+        cdf(i) = pdf(i);
+    else
+        cdf(i) = cdf(i-1) + pdf(i);
+    end
+    expactation = expactation + i * pdf(i);
 end
 
 %% visualization
-plot(prob)
+f1 = subplot(1,2,1);
+plot(pdf)
 title('第n抽出货概率')
-text(20,0.03,['期望抽数为',num2str(expactation)],'fontsize',18)
+text(10,0.025,['期望抽数为',num2str(expactation)],'fontsize',16)
 xlabel('抽数')
+set(f1,'position',[0.1,0.35,0.4,0.4])
+
+f2 = subplot(1,2,2);
+plot(cdf)
+title('累计分布')
+xlabel('抽数')
+set(f2,'position',[0.55,0.35,0.4,0.4])
+
 
